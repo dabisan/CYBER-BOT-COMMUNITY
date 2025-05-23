29ff3a8cdfdb7e4bfcc4cd7047f4778a305cc26e
dabisan <mehdisan21@gmail.com>
2025-05-23 16:46:02 +0100

    Update antibd.js

--- Script/events/antibd.js
+++ Script/events/antibd.js
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - পাগল ছাগল তুই নিকনেম চেঞ্জ করতে পারবি না 😹\n শুধু আমার বস উল্লাস চেঞ্জ করতে পারবে🖐`}, threadID);
+       return api.sendMessage({ body: `${info.name} - لا يوجد شيء أفضل من ذلك 😹\n ما هو أفضل ما عليك فعله🖐`}, threadID);
     }  
         }
