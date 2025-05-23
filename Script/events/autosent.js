a5b46174d881b8e32658fb240cf7cc19c804b35b
dabisan <mehdisan21@gmail.com>
2025-05-23 16:51:25 +0100

    Update autosent.js

--- Script/events/autosent.js
+++ Script/events/autosent.js
@@ -8,7 +8,7 @@ module.exports.config = {
 
 module.exports.run = async({ event, api, Threads, Users }) => {
 const moment = require("moment-timezone");
-time = moment.tz('Asia/Dhaka').format('HH:mm:ss');
+time = moment.tz('Africa/Algiers').format('HH:mm:ss');
 var cantsend = [];
     var allThread = global.data.allThreadID || [];
     if (time == "17:22:00") {
