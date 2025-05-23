7866679b769adcc1b1a319a6e7699e7f2878f56e
dabisan <mehdisan21@gmail.com>
2025-05-23 14:31:10 +0100

    Update Create.js

--- Script/commands/Create.js
+++ Script/commands/Create.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-  name: "create",
+  name: "انشاء",
   version: "1.0.",
   hasPermssion: 0,
   credits: "Islamick Chat",
@@ -12,14 +12,14 @@ module.exports.run = async ({api, event, args }) => {
 const axios = require('axios');
 const fs = require('fs-extra');
  let { threadID, messageID } = event;
-  let query = args.join("𝐒𝐮𝐜𝐜𝐞𝐬𝐟𝐮𝐥 𝐅𝐨𝐫 𝐘𝐨𝐮𝐫 𝐂𝐫𝐞𝐚𝐭𝐞 𝐈𝐦𝐠✨🌺");
-  if (!query) return api.sendMessage("𝖯𝗅𝖾𝖺𝗌𝖾 𝗎𝗌𝖾 ✓𝗀𝖾𝗇𝗆𝖺𝗀𝖾 <𝗍𝖾𝗑𝗍>", threadID, messageID);
+  let query = args.join("تم إنشاء الصورة بنجاح من أجلك ✨🌺");
+  if (!query) return api.sendMessage("يرجى استخدام ✓انشاء <النص>", threadID, messageID);
 let path = __dirname + `/cache/poli.png`;
   const poli = (await axios.get(`https://image.pollinations.ai/prompt/${query}`, {
     responseType: "arraybuffer",
   })).data;
   fs.writeFileSync(path, Buffer.from(poli, "utf-8"));
   api.sendMessage({
-    body: "𝐒𝐮𝐜𝐜𝐞𝐬𝐟𝐮𝐥 𝐅𝐨𝐫 𝐘𝐨𝐮𝐫 𝐂𝐫𝐞𝐚𝐭𝐞 𝐈𝐦𝐠✨🌺",
+    body: "تم إنشاء الصورة بنجاح لك ✨🌺",
     attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID);
 };
